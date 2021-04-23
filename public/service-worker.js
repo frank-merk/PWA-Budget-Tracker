891f2a0b6d09b5881e7f97e7e55d88e38a7edc66
Frank Merchlewitz <jlimhb@gmail.com>
2021-04-22 22:42:53 -0500

    updated boilerplate service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,68 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/favorites.html',
-    '/topic.html',
-    '/assets/css/style.css',
-    '/dist/app.bundle.js',
-    '/dist/favorites.bundle.js',
-    '/dist/topic.bundle.js',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+    '/index.js',
+    '/db.js',
+    '/style.css',
+    'manifest.webmanifest',
+    '/icons/icon-192x192.png',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
   ];
   
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Deleting Old Cache", key);
+            return caches.delete(key);
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
         })
       );
-    }
-  });
-  
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cachedResponse) => {
+        return fetch(event.request)
+          .then(nestResponse => {
+            if (nestResponse.status === 200) {
+              cachedResponse.put(event.request.url, nestResponse.clone());
+            }
+            return nestResponse;
+        }).catch(err => {
+          return cache.match(event.request);
+        })
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response =>{
+        return response || fetch(event.request)
+      })
+    })
+  )
+});
