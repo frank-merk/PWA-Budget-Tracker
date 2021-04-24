2b9f9271db4306cdf7b8d34281b88883b71eb90b
Frank Merchlewitz <jlimhb@gmail.com>
2021-04-23 21:21:23 -0500

    finished routing for bulk getAll() transactions, fixed port

--- public/db.js
+++ public/db.js
@@ -37,6 +37,27 @@ function checkDatabase() {
     const newStore = transaction.objectStore("storedItem");
     // put all the stored items into a new variable with the getAll method
     const getAll = newStore.getAll();
+
+        // on success, fetch the json from the api bulk transaction url
+        getAll.onsuccess = function() {
+            if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["storedItem"], "readwrite");
+                const newStore = transaction.objectStore("storedItem");
+                // clear all items in the pending transactions store when the function runs
+                newStore.clear();
+            });
+            }
+        };
 };
 
 // when the window is online, run checkdatabase to update
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
