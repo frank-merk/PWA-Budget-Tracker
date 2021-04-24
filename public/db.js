37c961bddbf66f60ae5d02a916db5d3bf1834f13
Frank Merchlewitz <jlimhb@gmail.com>
2021-04-23 21:10:44 -0500

    added two functions for saving offline records and for checking the database for all new transactions when the app comes back online

--- public/db.js
+++ public/db.js
@@ -13,4 +13,31 @@ request.onerror = (event) => {
     console.log("Error Alert!" + event.target.errorCode);
 };
 
-// 
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    // check cache offline
+    if (navigator.onLine) {
+        checkDatabase();
+    } 
+};
+
+// Adding data to DB
+function saveRecord(item) {
+    // new transaction created using the cache stored item
+    const transaction = db.transaction(["storedItem"], "readwrite");
+    // access the stored items
+    const newStore = transaction.objectStore("storedItem");
+    // save that new item to the the new store
+    newStore.add(item);
+  }
+
+function checkDatabase() {
+    const transaction = db.transaction(["storedItem"], "readwrite");
+    const newStore = transaction.objectStore("storedItem");
+    // put all the stored items into a new variable with the getAll method
+    const getAll = newStore.getAll();
+};
+
+// when the window is online, run checkdatabase to update
+window.addEventListener('online', checkDatabase);
